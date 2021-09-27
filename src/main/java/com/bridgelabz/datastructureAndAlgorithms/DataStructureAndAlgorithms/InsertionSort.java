00397adff1bb40ddf688bbb6c8f6a3df72525047
niharika-1999 <niharika.rao@ymedialabs.com>
2021-09-27 16:20:27 +0530

    Added:Reads in strings and prints them in sorted order using Insertion sort

---
+++ src/main/java/com/bridgelabz/datastructureAndAlgorithms/DataStructureAndAlgorithms/InsertionSort.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.datastructureAndAlgorithms.DataStructureAndAlgorithms;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class InsertionSort {
+	private static void insertionSort(String[] stringArray) 
+	{
+		int n = stringArray.length;
+        for (int i=1; i<n; i++)
+        {
+            String key = stringArray[i];
+            int j = i-1;
+            while (j>=0 && stringArray[j].compareTo(key)>0)
+            {
+            	stringArray[j+1] = stringArray[j];
+                j = j-1;
+            }
+            stringArray[j+1] = key;
+        }
+        for(int i=0;i<stringArray.length;i++)
+        {
+            System.out.print(stringArray[i]+" ");
+        }		
+	}
+	public static void main(String[] args) throws Exception
+	{
+		BufferedReader buffereader = new BufferedReader(new FileReader("C:\\Users\\Niharika Rao\\eclipse-workspace\\DataStructureAndAlgorithms\\src\\main\\java\\com\\bridgelabz\\datastructureAndAlgorithms\\DataStructureAndAlgorithms\\BinarySearch.txt"));
+        Scanner sc = new Scanner(System.in);
+        String line = null;
+        while ((line = buffereader.readLine()) != null) 
+        {
+          String[] splitArray = line.split(",");
+          insertionSort(splitArray);
+        }
+        sc.close();
+        buffereader.close();
+	}
+
+}
